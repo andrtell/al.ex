38fb11f6341a42c00f05e115de6d95c2d8034b5d
Andreas Tell <git@tell.nu>
2022-12-02 17:18:00 +0100

    add Stack DS

---
+++ lib/d/stack.ex
@@ -0,0 +1,29 @@
+defmodule Al.D.Stack do
+  def empty() do
+    []
+  end
+  
+  def is_empty?(s) do
+    s == []
+  end
+
+  def push(s, val) do
+    [val | s]
+  end
+
+  def pop([_ | s]) do
+    s
+  end
+
+  def pop([]) do
+    raise ArgumentError, "Stack is empty"
+  end
+
+  def top([val | _]) do
+    val
+  end
+
+  def top([]) do
+    raise ArgumentError, "Stack is empty"
+  end
+end
